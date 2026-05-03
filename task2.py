cf2a7ec12b36637c62e197767a55792f1c254e27
ShivamSolapure <solapureshivams@gmail.com>
2026-05-03 11:41:51 +0530

    initial commit

---
+++ task2.py
@@ -0,0 +1,12 @@
+num1=int(input("Enter First Number:"))
+num2=int(input("Enter Second Number:"))
+add= num1+num2
+print("Result Of Addition:",add)
+sub= num1-num2
+print("Result Of Subtraction:",sub)
+mul= num1*num2
+print("Result Of Multiplication:",mul)
+div= num1/num2
+print("Result Of Division:",div)
+mod= num1%num2
+print("Result Of Modulus:",mod)
